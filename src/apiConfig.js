5fd3aa9fa0aaa8ad16076ca8c1ada918ca6b2c5e
nstco29 <nstco29@gmail.com>
2019-01-17 16:11:29 -0500

    apiconfig modify

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://obscure-brook-97374.herokuapp.com/',
+  production: 'https://obscure-brook-97374.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
